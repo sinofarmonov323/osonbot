8454cb2433832eb2fae71b9a6c286db330391592
sinofarmonov323 <farmonovsino4@gmail.com>
2025-10-14 15:12:23 +0500

    updated

--- osonbot/__init__.py
+++ osonbot/__init__.py
@@ -1,1 +1,12 @@
 from .bot import Bot, Video, Photo, Audio, KeyboardButton, InlineKeyboardButton, URLKeyboardButton, RemoveKeyboardButton
+
+__all__ = [
+    "Bot",
+    "Photo",
+    "Video",
+    "Audio",
+    "KeyboardButton",
+    "InlineKeyboardButton",
+    "URLKeyboardButton",
+    "RemoveKeyboardButton"
+]
--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ setup(
     packages=find_packages(),
     requires=['httpx'],
     author="https://t.me/jackson_rodger",
-    description="A simple and lightweight Telegram bot framework using httpx"
+    description="A simple and lightweight Telegram bot framework using httpx",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["httpx"]
 )
