a83cb4da11554cb9f4c9181bcee47272ce292ca5
sinofarmonov323 <farmonovsino4@gmail.com>
2025-10-17 01:47:09 +0500

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osonbot",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
     requires=['httpx'],
     author="https://t.me/jackson_rodger",
