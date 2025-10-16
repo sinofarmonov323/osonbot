8d5f9856f81ccc76c24cb04053b32188aafd3c7a
sinofarmonov323 <farmonovsino4@gmail.com>
2025-10-16 14:05:51 +0500

    updated

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ setup(
     packages=find_packages(),
     requires=['httpx'],
     author="https://t.me/jackson_rodger",
-    description="A simple and lightweight Telegram bot framework using httpx",
+    description="A simple and lightweight Telegram bot framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sinofarmonov323/osonbot",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=["httpx"]
 )
