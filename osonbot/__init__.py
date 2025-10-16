ffefeefa792aa1e737f561f8a8dc94c013d3ece5
sinofarmonov323 <farmonovsino4@gmail.com>
2025-10-16 14:30:21 +0500

    updated

--- osonbot/__init__.py
+++ osonbot/__init__.py
@@ -1,8 +1,7 @@
-from .bot import Bot, Photo, Video, Audio, Voice, Sticker, KeyboardButton, RemoveKeyboardButton, InlineKeyboardButton, URLKeyboardButton, CreateTable
+from .bot import Bot, Photo, Video, Audio, Voice, Sticker, KeyboardButton, RemoveKeyboardButton, InlineKeyboardButton, URLKeyboardButton
 
 __all__ = [
     "Bot",
     "Photo", "Video", "Audio", "Voice", "Sticker",
-    "KeyboardButton", "RemoveKeyboardButton", "InlineKeyboardButton", "URLKeyboardButton",
-    "CreateTable"
+    "KeyboardButton", "RemoveKeyboardButton", "InlineKeyboardButton", "URLKeyboardButton"
 ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osonbot",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     requires=['httpx'],
     author="https://t.me/jackson_rodger",
